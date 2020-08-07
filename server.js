e5f6fd0c53b51d6dafa3ad25090fd210eaafc69d
nhcarrigan <nhcarrigan@gmail.com>
2020-08-07 11:11:34 -0700

    Ready to test deployment

---
+++ server.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const GMAIL_USER = process.env.GMAIL_USER;
+const GMAIL_PASS = process.env.GMAIL_PASS;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/contact", (req, res) => {
+  const smtpTrans = nodemailer.createTransport({
+    host: "smpt.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: GMAIL_USER,
+      pass: GMAIL_PASS,
+    },
+  });
+  const mailOpts = {
+    from: "contact form",
+    to: GMAIL_USER,
+    subject: req.body.subject,
+    text: `Dear Nicholas, \n You have received the following message from ${req.body.name}: \n ${req.body.message} \n Reply to them at ${req.body.email}`,
+  };
+  smtpTrans.sendMail(mailOpts, (error, response) => {
+    if (error) {
+      res.json({ Error: "Failed to send message" });
+    } else {
+      res.json({ Success: "Message sent!" });
+    }
+  });
+});
